9277ddfcb28b1daba5bd32b241e686b8b3285959
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2022-09-20 22:12:40 -0700

    Rename all projects to begin with 'ffmpeg-...'

--- ffmpeg-headers.bat
+++ ffmpeg-headers.bat
@@ -7,8 +7,11 @@ pushd %~dp0
 echo Installing FFMPEG headers into "%OutDir%..\include" . . .
 
 for /F "usebackq tokens=*" %%H in ("ffmpeg-headers.lst") do (
-    xcopy /s /r /y "%%H" "%OutDir%..\include\%%H*" >nul
+    xcopy /s /r /y "%%H" "%OutDir%..\include\%%H*"
     attrib +R "%OutDir%..\include\%%H"
 )
+
+xcopy /r /y ffmpeg-headers.avconfig.h.in "%OutDir%..\include\libavutil\avconfig.h*"
+attrib +R "%OutDir%..\include\libavutil\avconfig.h"
 
 popd
