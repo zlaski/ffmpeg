4d74416a8f8fc3e49a103cccbc15c0d114e9a9b2
Ziemowit Łąski <zlaski@ziemas.net>
2022-12-22 13:18:54 -0800

    Update build-cygwin.h

--- build-cygwin.sh
+++ build-cygwin.sh
@@ -1,18 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
 
 SCRIPT=`basename $0 .sh`
 SCRIPT_DIR=$(realpath $(dirname $0))
 pushd $SCRIPT_DIR
 
-echo CMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX
-echo BUILD_SHARED_LIBS=$BUILD_SHARED_LIBS
-
+echo "################### BUILDING $0 ####################"
+    
 rm -rf .build || exit 1
 mkdir -p .build || exit 1
 cd .build || exit 1
 
-../configure --prefix=$CMAKE_INSTALL_PREFIX || exit 1
-make || exit 1
-make install || exit 1
+SHELL='/bin/bash -x' ../configure --prefix=$CMAKE_INSTALL_PREFIX || exit 1
+    
+make SHELL='/bin/bash -x' || exit 1
+make install SHELL='/bin/bash -x' || exit 1
 
 popd
