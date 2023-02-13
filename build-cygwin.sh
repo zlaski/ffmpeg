88694f744d5e5099357585282da91be568171714
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-02-13 00:48:10 -0800

    Update build-cygwin.sh

--- build-cygwin.sh
+++ build-cygwin.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR=$(realpath $(dirname $0))
 # must delete .build directory to trigger a build    
 test -d $SCRIPT_DIR/.build && exit 0
 
+CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:-/tmp/ffmpeg-build}
+
 pushd $SCRIPT_DIR
 
 echo "################### BUILDING $0 ####################"
@@ -12,8 +14,10 @@ echo "################### BUILDING $0 ####################"
 mkdir -p .build || exit 1
 cd .build || exit 1
 
-SHELL='/bin/bash -x' ../configure --prefix=$CMAKE_INSTALL_PREFIX --enable-debug=gdb \
-    --disable-doc --enable-static --disable-shared --pkg-config-flags=--static || exit 1
+SHELL='/bin/bash -x' ../configure --prefix=$CMAKE_INSTALL_PREFIX --enable-debug=3 \
+    --enable-static --disable-shared --pkg-config-flags=--static \
+    --disable-optimizations --extra-cflags=-Og --extra-cxxflags=-Og \
+    --disable-asm || exit 1
     
 make SHELL='/bin/bash -x' || exit 1
 make install SHELL='/bin/bash -x' || exit 1
